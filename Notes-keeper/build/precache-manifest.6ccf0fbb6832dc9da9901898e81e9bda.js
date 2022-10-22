b3b67028ccd0fb78d33ea1e0e746e6ab7ee7c315
Vasu Gambhir <vasugambhir15@gmail.com>
2022-10-23 00:18:00 +0530

    adding notes-keeper

---
+++ Notes-keeper/build/precache-manifest.6ccf0fbb6832dc9da9901898e81e9bda.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6b07cd8933c5fb149fd8fb8e2f2bcb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a81652551a1bdcc7fa69",
+    "url": "/static/js/2.c80056b5.chunk.js"
+  },
+  {
+    "revision": "1218e4abc33786ab6ec5",
+    "url": "/static/js/main.e50a2351.chunk.js"
+  },
+  {
+    "revision": "84832877ed76605d723e",
+    "url": "/static/js/runtime-main.aa743321.js"
+  }
+]);
